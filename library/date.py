cd748cb89a9d1f7fa3e6f09e10b1e6103049d0e7
Pravin Mali <rhcepravin@gmail.com>
2018-07-05 11:25:21 +0530

    Update date.py

--- library/date.py
+++ library/date.py
@@ -3,11 +3,15 @@
 import datetime
 import json
 
+def main():
+module = AnsibleModule()
+
 date = str(datetime.datetime.now())
 print json.dumps({
     "time" : date
 })
 
+module.exit_json(changed=True)
 
 from ansible.module_utils.basic import AnsibleModule
 if __name__ == '__main__':
