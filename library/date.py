734d4cfa8b6ddc3925c294b6ad0da4a8c19a1b6c
Pravin Mali <rhcepravin@gmail.com>
2018-07-05 11:28:44 +0530

    Update date.py

--- library/date.py
+++ library/date.py
@@ -13,6 +13,6 @@ print json.dumps({
 
 module.exit_json(changed=True)
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import *
 if __name__ == '__main__':
     main()
